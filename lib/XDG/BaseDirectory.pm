b0cf2db03874fb0f0da104732d21a63aaf4ed403
Jonathan Stowe <jns+git@gellyfish.co.uk>
2015-09-04 19:57:05 +0100

    Post GLR changes

--- lib/XDG/BaseDirectory.pm
+++ lib/XDG/BaseDirectory.pm
@@ -66,7 +66,7 @@ but will always prefer C<data-home>.
 
     method data-dirs() {
         if ! @!data-dirs.elems {
-            @!data-dirs = $.data-home, (%*ENV<XDG_DATA_DIRS> || '/usr/local/share:/usr/share').split(':').map({ $_.IO });
+            @!data-dirs = ($.data-home, (%*ENV<XDG_DATA_DIRS> || '/usr/local/share:/usr/share').split(':').map({ $_.IO })).flat;
         }
         @!data-dirs;
     }
@@ -102,7 +102,7 @@ colons.
 
     method config-dirs() {
         if ! @!config-dirs.elems {
-            @!config-dirs = $.config-home, (%*ENV<XDG_CONFIG_DIRS> || '/etc/xdg' ).split(':').map({ $_.IO });
+            @!config-dirs = ($.config-home, (%*ENV<XDG_CONFIG_DIRS> || '/etc/xdg' ).split(':').map({ $_.IO })).flat;
         }
         @!config-dirs;
     }
