2bc21d8bb86a2e527300eb20c5f40a37ec336dc8
Jonathan Stowe <Jonathan.Stowe@net-a-porter.com>
2019-01-21 09:39:09 +0000

    Up version, add changes etc

--- lib/XDG/BaseDirectory.pm
+++ lib/XDG/BaseDirectory.pm
@@ -34,7 +34,7 @@ methods that return a string path in that module return an L<IO::Path> here.
 
 =end pod
 
-class XDG::BaseDirectory:ver<0.0.8>:auth<github:jonathanstowe>:api<1.0> {
+class XDG::BaseDirectory:ver<0.0.9>:auth<github:jonathanstowe>:api<1.0> {
 
 =begin pod
 
